4b3997b306b34b648f8d4e66df9090372dc0ef7e
chensongxian <1164077611@qq.com>
2018-04-10 18:01:08 +0800

    对象池

--- src/main/java/com/csx/pool/PooledObjectState.java
+++ src/main/java/com/csx/pool/PooledObjectState.java
@@ -31,7 +31,22 @@ public enum PooledObjectState {
     /**
      * 不在队列中，当前正在验证。当对象从池中被借出，在配置了testOnBorrow的情况下，对像从队列移除和进行预分配的时候会进行验证
      */
-    VALIDATION_PREALLOCATED;
-
-
+    VALIDATION_PREALLOCATED,
+    /**
+     * 不在队列中，正在进行验证。从池中借出对象时，从队列移除对象时会先进行测试。
+     * 返回到队列头部的时候应该做一次完整的验证
+     */
+    VALIDATION_RETURN_TO_HEAD,
+    /**
+     * 回收或验证失败，将销毁
+     */
+    INVALID,
+    /**
+     * 即将无效
+     */
+    ABANDONED,
+    /**
+     * 返回池中
+     */
+    RETURNING
 }
